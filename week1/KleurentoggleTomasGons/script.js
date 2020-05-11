7807761f086d37e641315bf1569984ba56112178
Tomas Gons <tomasgons@gmail.com>
2020-05-11 11:48:42 +0200

    5

--- week1/KleurentoggleTomasGons/script.js
+++ week1/KleurentoggleTomasGons/script.js
@@ -1,30 +1,24 @@
 // hamburgermenu
 const burgerMenu = document.querySelector(".burger");
-
 const wrapMenu = document.querySelector(".wrapper");
 const grootMenu = document.querySelector(".menu");
-
 const bodyColor = document.querySelector("body");
-
 const menuLinks = document.querySelectorAll("input");
+const backgroundText = document.querySelector("#colortext");
 
-// dit maakt eventlsnts vast aan de button en zegt wat ze moeten doen
+// dit maakt eventlisteners vast aan de button en zegt wat ze moeten doen
 
 document.querySelectorAll("input").forEach((Btn) => {
   Btn.addEventListener("click", () => {
-    // console.log(Btn.innerHTML);
-
     bodyColor.className = Btn.id;
+    backgroundText.innerHTML = Btn.id;
   });
 });
-
 // verander classname van menu-onderdelen
-
 menuLinks.forEach(function (Btn) {
   Btn.className = Btn.id;
 });
 
-// toggle menu
 const toggleGrootMenu1 = function () {
   burgerMenu.addEventListener("click", function () {
     grootMenu.classList.toggle("expanded");
@@ -41,20 +35,25 @@ const hideMenu = function () {
 };
 hideMenu();
 
-// hiermee veranderd=t de kleur op basis van toetsenbord
-
+// hiermee verandert de kleur op basis van toetsenbord
 window.addEventListener("keydown", function (e) {
   if (e.code == "Digit1" || e.code == "Numpad1") {
     bodyColor.className = "grey";
+    backgroundText.innerHTML = bodyColor.className;
   } else if (e.code == "Digit2" || e.code == "Numpad2") {
     bodyColor.className = "red";
+    backgroundText.innerHTML = bodyColor.className;
   } else if (e.code == "Digit3" || e.code == "Numpad3") {
     bodyColor.className = "blue";
+    backgroundText.innerHTML = bodyColor.className;
   } else if (e.code == "Digit4" || e.code == "Numpad4") {
     bodyColor.className = "green";
+    backgroundText.innerHTML = bodyColor.className;
   } else if (e.code == "Digit5" || e.code == "Numpad5") {
     bodyColor.className = "yellow";
+    backgroundText.innerHTML = bodyColor.className;
   } else if (e.code == "Digit6" || e.code == "Numpad6") {
     bodyColor.className = "purple";
+    backgroundText.innerHTML = bodyColor.className;
   }
 });
