6c9d42c2e4bbac278a6e655ba2cb9b8dbe6b6006
Tomas Gons <tomasgons@gmail.com>
2020-05-13 09:59:25 +0200

    6

---
+++ week2/dag 3/W2D3-1.js
@@ -0,0 +1,28 @@
+// A // zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => {
+  console.log("Joe, ik rock de arrow functions!");
+};
+ikRockArrowFunctions();
+
+// B
+
+const fivePlusSeven = () => 5 + 7;
+fivePlusSeven();
+
+// C
+
+() => 1 + 2;
+
+// D
+
+const myFunction = (a, b) => a + b;
+console.log(myFunction(5, 7));
+
+// E
+
+const addFive = (a) => a + 5;
+console.log(addFive(3));
+
+// F
+const createObject = () => ({ greeting: "hoi" });
+console.log(createObject());
